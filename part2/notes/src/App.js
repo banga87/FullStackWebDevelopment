390d5ccdd4d53db99599db501000e1152661d8f0
Angus McDonald <angusroymcdonald@gmail.com>
2023-07-27 14:03:44 +1000

    added an error message bar to notes + some simple styling

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import Note from './components/Note'
+import Notification from './components/Notification';
 import noteService from './services/noteService';
 
 const App = () => {
   const [ notes, setNotes ] = useState([])
   const [ newNote, setNewNote] = useState('')
   const [ showAll, setShowAll ] = useState(true)
+  const [ errorMessage, setErrorMessage ] = useState('')
 
 
   useEffect(() => {
@@ -30,9 +32,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNotes))
       })
       .catch(error => {
-        alert(
-          `the note ${note.content} was already deleted from server`
+        setErrorMessage(
+          `Note ${note.content} was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -70,6 +75,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={ errorMessage } />
       <div>
         <button onClick={()=> setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
