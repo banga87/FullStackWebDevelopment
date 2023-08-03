e726c80b878de5a47e2d697f0bb02032c4757304
BingBong <angusroymcdonald@gmail.com>
2023-08-03 17:50:37 +1000

    3.4: Phonebook backend step4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,32 @@
-const persons = require('./data')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 
 const root = '/api/persons'
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
 
 //PHONEBOOK INFO
 app.get(`/info`, (request, response) => {
@@ -38,6 +60,13 @@ app.get(`${root}/:id`, (request, response) => {
 // CREATE A PERSON
 
 // DELETE A PERSON
+app.delete(`${root}/:id`, (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+
+})
 
 // PORT & SERVER
 const PORT = 3001;
